72219cf1903a410695750906dfb2b7640c01ec6f
hansenu <ucabhansen@hotmail.com>
2020-09-16 10:16:30 -0400

    Assignment 12
    Assignment 12. Happy finally got it to work. Forgot the ) after the Irish

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,1 +1,7 @@
-alert("Hi, there!");
+alert("Hi, sexy!");
+function myFunction() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML=
+		result;
+	}
